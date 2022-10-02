5a8447545106be7a40249d225e29c304c9ee3d6c
onurhanas <113357599+onurhanas@users.noreply.github.com>
2022-10-02 22:05:19 +0300

    SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int month= DateTime.Now.Month;
+          switch (month)
+          {
+            case 1:
+            Console.WriteLine("Ocak ayındasınız");
+            break;
+            case 10:
+            Console.WriteLine("Ekim ayındasınız");
+            break;
+
+            
+            default:
+            Console.WriteLine("Yanlış veri girdiniz");
+            break;
+
+          }
+          switch (month)
+
+          {
+            case 12:
+            case 1:
+            case 2:
+            Console.WriteLine("Kış ayındasınız");
+            break;
+            case 9:
+            case 10:
+            case 11:
+            Console.WriteLine("Sonbahar ayındasınız");
+            break;
+            
+            default:
+            break;
+          }
+
+        }
+    }
+}
